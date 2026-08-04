b4f23808801a49064fd7f8c874093e102f8f30e7
huka173 <4g5gr55y55y@gmail.com>
2026-08-04 19:36:26 +0300

    Added new tests for List

--- tests/TestList/TestList.cpp
+++ tests/TestList/TestList.cpp
@@ -1,6 +1,5 @@
 #include <gtest/gtest.h>
 #include "List/List.h"
-
 TEST(List, TestInitializerList) {
     List<int> list{ 1, 2, 3, 4 };
     ASSERT_EQ(list.size(), 4);
@@ -32,4 +31,57 @@ TEST(List, TestSize) {
     EXPECT_EQ(list.size(), 0);
     list.pop_back();
     EXPECT_EQ(list.size(), 0);
+}
+
+TEST(List, TestPushFront) {
+    List<int> list;
+    list.push_front(1);
+    EXPECT_EQ(list.front(), 1);
+    list.push_front(2);
+    EXPECT_EQ(list.front(), 2);
+    list.push_front(3);
+    EXPECT_EQ(list.front(), 3);
+    EXPECT_EQ(list.back(), 1);
+}
+
+TEST(List, TestClear) {
+    List<int> list{ 1, 2, 3, 4, 5 };
+    list.clear();
+    ASSERT_EQ(list.size(), 0);
+}
+
+TEST(List, TestEmpty) {
+    List<int> list;
+    ASSERT_TRUE(list.empty());
+}
+
+TEST(List, TestPopFront) {
+    List<int> list{ 1, 2, 3 };
+    EXPECT_EQ(list.front(), 1);
+    list.pop_front();
+    EXPECT_EQ(list.front(), 2);
+    list.pop_front();
+    EXPECT_EQ(list.front(), 3);
+    list.pop_front();
+    EXPECT_EQ(list.size(), 0);
+}
+
+TEST(List, TestForwardDirection) {
+    List<int> list{ 22, 33, 44 };
+    int assertArr[3]{ 22, 33, 44 };
+
+    size_t i{};
+    for (auto *it{ list.begin() }; it != nullptr; it = it->next, ++i) {
+        EXPECT_EQ(assertArr[i], it->value);
+    }
+}
+
+TEST(List, TestBackwardDirection) {
+    List<int> list{ 22, 33, 44 };
+    int assertArr[3]{ 22, 33, 44 };
+
+    int i{ 2 };
+    for (auto *it{ list.end() }; it != nullptr; it = it->prev, --i) {
+        ASSERT_EQ(assertArr[i], it->value);
+    }
 }
